908d612d3c2f46639542b29561a55585d6aa2234
saqmal <52063447+saqmal@users.noreply.github.com>
2019-10-02 18:05:20 +0900

    Add files via upload
    for setup

---
+++ 201910_tutorialsetup.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+echo #########################
+echo package update & install
+echo #########################
+
+set +x 
+
+apt upgrade
+apt install -y binutils build-esssential sysstat strace 
+
+set -x
+
+echo #########################
+echo setup sysstat
+echo #########################
+
+set +x 
+
+sed -i s/'ENABLED="false"'/'ENABLED="true"'/g /etc/default/sysstat
+service sysstat start
+
+set -x
+
+echo #########################
+echo clone cutorial
+echo #########################
+
+set +x 
+
+cd ../
+git clone https://github.com/satoru-takeuchi/linux-in-practice/
+
+
+exit 0
